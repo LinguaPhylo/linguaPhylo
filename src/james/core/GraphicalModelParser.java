f213d31ec519f11a59b290fde8b1db877f325eaf
alexeidrummond <alexei.drummond@gmail.com>
2020-02-04 07:51:00 +1300

    Starting the parser

---
+++ src/james/core/GraphicalModelParser.java
@@ -0,0 +1,13 @@
+package james.core;
+
+import james.graphicalModel.GenerativeDistribution;
+import james.graphicalModel.Value;
+
+import java.util.Map;
+
+public class GraphicalModelParser {
+
+    Map<String, Value> dictionary;
+
+    //Map<Genera>
+}
