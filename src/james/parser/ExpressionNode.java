4bd217b0e940d04e0ca2a995395963e927c9bccd
Remco Bouckaert <higgs.ml@gmail.com>
2020-02-19 14:40:30 +1300

    some ideas for dealing with types #7

--- src/james/parser/ExpressionNode.java
+++ src/james/parser/ExpressionNode.java
@@ -4,29 +4,32 @@ import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.function.BinaryOperator;
-import java.util.function.Function;
+import java.util.function.*;
 
 import james.graphicalModel.DeterministicFunction;
 import james.graphicalModel.GraphicalModelNode;
 import james.graphicalModel.Parameterized;
 import james.graphicalModel.Value;
+import james.graphicalModel.types.DoubleArray2DValue;
+import james.graphicalModel.types.DoubleArrayValue;
 import james.graphicalModel.types.DoubleValue;
+import james.graphicalModel.types.IntegerValue;
 
 /** anonymous container holding a DeterministicFunction **/
-public class ExpressionNode extends Value implements Parameterized {
+public class ExpressionNode<T> extends DeterministicFunction<T> implements Parameterized {
 	String expression;
 	Map<String, Value> params;
+	Value [] inputValues;
 	BinaryOperator func;
 	
 	public ExpressionNode(String expression, BinaryOperator func, Value ...values) {
-		super(null, new DoubleValue(null, (Double) func.apply(values[0].value(), values[1].value())));
 		this.expression = expression;
 		this.func = func;
 		params = new LinkedHashMap<>();
 		for (Value value : values) {
 			params.put(value.getId(), value);
 		}
+		inputValues = values;
 	}
 
 	@Override
@@ -35,6 +38,11 @@ public class ExpressionNode extends Value implements Parameterized {
 		return null;
 	}
 
+	@Override
+	public Value<T> apply() {
+		return new Value(null, func.apply(inputValues[0].value(), inputValues[1].value()));
+	}
+	
 	@Override
 	public Map<String, Value> getParams() {
 		return new LinkedHashMap<>(params);
@@ -58,9 +66,127 @@ public class ExpressionNode extends Value implements Parameterized {
 
 	
 	// binary operators
-	static BinaryOperator<Double> plus() {
+	// OPTION 0: make plus() type specific and let parser pick the right one
+	static BinaryOperator<Double> plusDD() {
         return (a, b) -> a + b;
     }
+	// TODO: implement many more operators 
+
+	
+	// OPTION 1: make plus() a BinaryOperator<double> and make ExpressionNode deal with types
+	// TODO: requires re-engineering of apply()
+
+	// OPTION 2: make plus() a BinaryOperator<Value> and deal with types afterwards
+	static BinaryOperator<Value> plus() {
+      return (a, b) -> {
+    	  if (a instanceof DoubleArray2DValue && b instanceof DoubleArray2DValue) {
+    		  Double [][] va = (Double [][]) a.value();
+    		  Double [][] vb = (Double [][]) b.value();
+    		  Double [][] r = new Double [va.length][va[0].length];
+    		  for (int i = 0; i < va.length; i++) {
+    			  for (int j = 0; j < va.length; j++) {
+    				  
+    				  r[i][j] = va[i][j] + vb[i][j];
+    			  }
+    		  }
+    		  return new DoubleArray2DValue("", r);
+    	  }
+    	  if (a instanceof DoubleArrayValue && b instanceof DoubleArrayValue) {
+    		  Double [] va = (Double []) a.value();
+    		  Double [] vb = (Double []) b.value();
+    		  Double [] r = new Double [va.length];
+    		  for (int i = 0; i < va.length; i++) {
+    			  r[i] = va[i] + vb[i];
+    		  }
+    		  return new DoubleArrayValue("", r);
+    	  }
+    	  if (a instanceof DoubleValue && b instanceof DoubleValue) {
+    		  Double va = (Double) a.value();
+    		  Double vb = (Double) b.value();
+    		  Double r = va + vb;
+    		  return new DoubleValue("", r);
+    	  }
+    	  if (a instanceof IntegerValue && b instanceof IntegerValue) {
+    		  Integer va = (Integer) a.value();
+    		  Integer vb = (Integer) b.value();
+    		  Integer r = va + vb;
+    		  return new IntegerValue("", r);
+    	  }
+    	  // TODO: etc. etc. 32 more combinations to go
+    	  return null;
+      };
+   }	
+	static BinaryOperator<Double[]> plusDADA() {
+        return (a, b) -> {
+        	Double [] result = new Double[a.length];
+        	for (int i = 0; i < a.length; i++) {
+        		result[i] = a[i] + b[i % b.length];
+        	}
+        	return result;
+        };
+    }
+//	static BiFunction<Double[], Double, Double[]> plusDAD() {
+//        return (a, b) -> {
+//        	Double [] result = new Double[a.length];
+//        	for (int i = 0; i < a.length; i++) {
+//        		result[i] = a[i] + b;
+//        	}
+//        	return result;
+//        };
+//    }
+//	static BiFunction<Double, Integer, Double> plusDI() {
+//        return (a, b) -> a + b;
+//    }
+//	static BinaryOperator<Integer> plusII() {
+//        return (a, b) -> a + b;
+//    }
+	static BinaryOperator<Integer[]> plusIAIA() {
+        return (a, b) -> {
+        	Integer [] result = new Integer[a.length];
+        	for (int i = 0; i < a.length; i++) {
+        		result[i] = a[i] + b[i % b.length];
+        	}
+        	return result;
+        };
+    }
+	static BiFunction<Double[], Integer[], Double[]> plusDAIA() {
+        return (a, b) -> {
+        	Double [] result = new Double[a.length];
+        	for (int i = 0; i < a.length; i++) {
+        		result[i] = a[i] + b[i % b.length];
+        	}
+        	return result;
+        };
+    }
+//	static BiFunction<Integer[], Integer, Integer[]> plusIAI() {
+//        return (a, b) -> {
+//        	Integer [] result = new Integer[a.length];
+//        	for (int i = 0; i < a.length; i++) {
+//        		result[i] = a[i] + b;
+//        	}
+//        	return result;
+//        };
+//    }
+//	static BiFunction<Integer[], Double, Double[]> plusIAD() {
+//        return (a, b) -> {
+//        	Double [] result = new Double[a.length];
+//        	for (int i = 0; i < a.length; i++) {
+//        		result[i] = a[i] + b;
+//        	}
+//        	return result;
+//        };
+//    }
+//	static BiFunction<Double, DeterministicFunction<Double>, Double> plusDF() {
+//        return (a, b) -> a + b.apply().value();
+//    }
+//	static BiFunction<Integer, DeterministicFunction<Double>, Double> plusIF() {
+//        return (a, b) -> a + (double) b.apply().value();
+//    }
+//	static BiFunction<DeterministicFunction<Double>, DeterministicFunction<Double>, Double> plusFF() {
+//        return (a, b) -> a.apply().value() + b.apply().value();
+//    }
+
+	
 	static BinaryOperator<Double> minus() {
         return (a, b) -> a - b;
     }
@@ -106,6 +232,5 @@ public class ExpressionNode extends Value implements Parameterized {
 	static BinaryOperator<Double> mod() {
         return (a, b) -> a % b;
     }
-	
 
 }
