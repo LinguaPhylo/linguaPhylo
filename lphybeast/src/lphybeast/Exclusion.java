d45f740b963d31a8928cbdee7a5bb13e651b80f4
Walter <walterxie@users.noreply.github.com>
2020-09-23 09:13:44 +1200

    fix LPhyBEAST to exclude Taxa interface but not Alignment

--- lphybeast/src/lphybeast/Exclusion.java
+++ lphybeast/src/lphybeast/Exclusion.java
@@ -1,6 +1,8 @@
 package lphybeast;
 
 import lphy.core.functions.*;
+import lphy.evolution.Taxa;
+import lphy.evolution.alignment.Alignment;
 import lphy.evolution.alignment.CharSetAlignment;
 import lphy.graphicalModel.Generator;
 import lphy.graphicalModel.Value;
@@ -15,7 +17,9 @@ public class Exclusion {
     public static boolean isExcludedValue(Value<?> val) {
         Object ob = val.value();
         // ignore all String: d = nexus(file="Dengue4.nex");
-        return ob instanceof String || ob instanceof String[] || ob instanceof CharSetAlignment;
+        return ob instanceof String || ob instanceof String[] || ob instanceof CharSetAlignment ||
+                // exclude the value returned by taxa (and ages) functions
+                ( ob instanceof Taxa && !(ob instanceof Alignment) ) ;
     }
 
     public static boolean isExcludedGenerator(Generator generator) {
