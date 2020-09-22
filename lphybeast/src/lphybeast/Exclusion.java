05c6bc08ba21ce9e80698c30fc6cc451a351e50d
Walter <walterxie@users.noreply.github.com>
2020-09-23 09:33:42 +1200

    add comments to Exclusion

--- lphybeast/src/lphybeast/Exclusion.java
+++ lphybeast/src/lphybeast/Exclusion.java
@@ -9,15 +9,18 @@ import lphy.graphicalModel.Value;
 
 /**
  * Utils class to exclude {@link lphy.graphicalModel.Value}
- * or {@link lphy.graphicalModel.Generator}.
+ * or {@link lphy.graphicalModel.Generator} to skip the validation
+ * so not to throw UnsupportedOperationException
+ * in either <code>BEASTContext#valueToBEAST(Value)<code/> or
+ * <code>BEASTContext#generatorToBEAST(Value, Generator)<code/>.
  * @author Walter Xie
  */
 public class Exclusion {
 
     public static boolean isExcludedValue(Value<?> val) {
         Object ob = val.value();
-        // ignore all String: d = nexus(file="Dengue4.nex");
-        return ob instanceof String || ob instanceof String[] || ob instanceof CharSetAlignment ||
+        return ob instanceof String || ob instanceof String[] || // ignore all String: d = nexus(file="Dengue4.nex");
+                ob instanceof CharSetAlignment || // CharSetAlignment itself has no sequences
                 // exclude the value returned by taxa (and ages) functions
                 ( ob instanceof Taxa && !(ob instanceof Alignment) ) ;
     }
