3101579912bd8fd592f701bd63da343d74dd9144
Walter <walterxie@users.noreply.github.com>
2020-09-28 15:48:57 +1300

    allow StringArray in charset arg in nexus func #55

--- lphy/src/lphy/core/functions/Nexus.java
+++ lphy/src/lphy/core/functions/Nexus.java
@@ -11,10 +11,14 @@ import lphy.graphicalModel.DeterministicFunction;
 import lphy.graphicalModel.GeneratorInfo;
 import lphy.graphicalModel.ParameterInfo;
 import lphy.graphicalModel.Value;
+import lphy.graphicalModel.types.StringArrayValue;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * data = nexus(file="primate.nex");
@@ -36,12 +40,15 @@ public class Nexus extends DeterministicFunction<Alignment> {
     public Nexus(@ParameterInfo(name = "file", description = "the name of Nexus file.") Value<String> fileName,
                  @ParameterInfo(name = "part", description = "the name of selected partition in Nexus, " +
                          "if none then return the full alignment.", optional=true) Value<String> part,
-                 @ParameterInfo(name = "charset", description = "the charset defined by Nexus syntax, " +
-                         "but cannot use with argument 'part' together.", optional=true) Value<String> charset,
+                 // charset=["1-629\3", "2-629\3", "3-629\3"] or charset="1-629\3"
+                 @ParameterInfo(name = "charset", description = "the charset(s) defined by Nexus syntax, " +
+                         "but cannot use with argument 'part' together.", optional=true) Value<?> charsets,
                  @ParameterInfo(name = "ageDirection", description = "age direction which is either forward (dates) or backward (ages).",
                          optional=true) Value<String> ageDirection,
                  @ParameterInfo(name = "ageRegex", description = "Java regular expression to extract dates.",
-                         optional=true) Value<String> regx ) {
+                         optional=true) Value<String> regx) {
+//                 @ParameterInfo(name = "options", description = "the map containing optional arguments and their values for reuse.",
+//                         optional=true) Value<Map> options) {
 
 
         if (fileName == null) throw new IllegalArgumentException("The file name can't be null!");
@@ -53,12 +60,26 @@ public class Nexus extends DeterministicFunction<Alignment> {
         regxParamName = getParamName(4);
 
         setParam(fileParamName, fileName);
+
+        // options overwrites other optional inputs
+//        if (options != null) {
+//            // convert from lphy map to java map
+//            java.util.Map<String, String> optMap = (java.util.Map<String, String>) options.value().apply().value();
+//
+//            for (java.util.Map.Entry<String, String> entry : optMap.entrySet()) {
+//                setParam(entry.getKey(), new Value(null, entry.getValue()));
+//                //
+//                if ( ! (entry.getKey().equals(ageDirectionParamName) || entry.getKey().equals(regxParamName) ) )
+//                        throw new IllegalArgumentException("Invalid arguments in options ! " + entry.toString());
+//            }
+//        }
+
         if (part != null) setParam(partParamName, part);
-        if (charset != null) setParam(charsetParamName, charset);
+        if (charsets != null) setParam(charsetParamName, charsets);
         if (ageDirection != null) setParam(ageDirectionParamName, ageDirection);
         if (regx != null) setParam(regxParamName, regx);
 
-        if (part != null && charset != null)
+        if (part != null && charsets != null)
             throw new IllegalArgumentException("Argument 'part' and 'charset' cannot use together");
     }
 
@@ -67,13 +88,18 @@ public class Nexus extends DeterministicFunction<Alignment> {
 
         Value<String> fileName = getParams().get(fileParamName);
         Value<String> part = getParams().get(partParamName);
-        Value<String> charset = getParams().get(charsetParamName);
         Value<String> ageDirection = getParams().get(ageDirectionParamName);
         Value<String> regx = getParams().get(regxParamName);
 
         final Path nexFile = Paths.get(fileName.value());
         NexusParser parser = new NexusParser(nexFile);
-        String tipCalibType = ageDirection == null ? null : ageDirection.value();
+        String ageDirectionStr = ageDirection == null ? null : ageDirection.value();
+
+        List<List<CharSetBlock>> charsetsList = new ArrayList<>();
+        Value<?> charset = getParams().get(charsetParamName);
+        if (charset != null)
+            charsetsList = parseCharsets(parser, charset);
+
 
         //TODO implement Alignment ?
 
@@ -82,25 +108,41 @@ public class Nexus extends DeterministicFunction<Alignment> {
             // must be CharSetAlignment
             if (cachedAlignment == null || !currentFileName.equalsIgnoreCase(fileName.value()) ||
                     !(cachedAlignment instanceof CharSetAlignment) ) {
-                cachedAlignment = parser.getLPhyAlignment(false, tipCalibType);
+                cachedAlignment = parser.getLPhyAlignment(false, ageDirectionStr);
                 currentFileName = fileName.value();
             }
             a = ((CharSetAlignment)  cachedAlignment).getPartAlignment(part.value());
 
-        } else if (charset != null) {
+        } else if (charsetsList.size() == 1) {
             // cache SimpleAlignment as parent alignment, and apply charset
             if (cachedAlignment == null || !currentFileName.equalsIgnoreCase(fileName.value()) ||
                     !(cachedAlignment instanceof SimpleAlignment) ) {
                 // ignore charset in Nexus file
-                cachedAlignment = parser.getLPhyAlignment(true, tipCalibType);
+                cachedAlignment = parser.getLPhyAlignment(true, ageDirectionStr);
                 currentFileName = fileName.value();
             }
-            List<CharSetBlock> charSetBlocks = parser.getImporter().getCharSetBlocks(charset.value());
-            a = CharSetAlignment.getPartition(charSetBlocks, (SimpleAlignment) cachedAlignment);
+            a = CharSetAlignment.getPartition(charsetsList.get(0), (SimpleAlignment) cachedAlignment);
+
+        } else if (charsetsList.size() > 1) {
+
+            if (cachedAlignment == null || !currentFileName.equalsIgnoreCase(fileName.value()) ||
+                    !(cachedAlignment instanceof CharSetAlignment) ) {
+                // ignore charset in Nexus file
+
+                Map<String, List<CharSetBlock>> charsetMap = new TreeMap<>();
+                for (int i = 0; i < charsetsList.size(); i++)
+                    charsetMap.put(Integer.toString(i+1), charsetsList.get(i));
+                SimpleAlignment parent = (SimpleAlignment) parser.getLPhyAlignment(true, ageDirectionStr);
+                cachedAlignment = new CharSetAlignment(charsetMap, parent);
+
+                currentFileName = fileName.value();
+            }
+
+            a = cachedAlignment;
 
         } else {
             // must be SimpleAlignment
-            a = parser.getLPhyAlignment(true, tipCalibType);
+            a = parser.getLPhyAlignment(true, ageDirectionStr);
         }
 
         if (regx != null) {
@@ -110,6 +152,24 @@ public class Nexus extends DeterministicFunction<Alignment> {
         }
 
         return new Value<>(a, this);
+    }
+
+    private List<List<CharSetBlock>> parseCharsets(final NexusParser parser, Object value) {
+        List<List<CharSetBlock>> charsetsList = new ArrayList<>();
+
+        if (value instanceof StringArrayValue) {
+            String[] strs = ((StringArrayValue) value).value();
+            for (int i = 0; i < strs.length; i++) {
+                List<CharSetBlock> charSetBlocks = parser.getImporter().getCharSetBlocks(strs[i]);
+                charsetsList.add(charSetBlocks);
+            }
+
+        } else { // String
+            List<CharSetBlock> charSetBlocks = parser.getImporter().getCharSetBlocks(value.toString());
+            charsetsList.add(charSetBlocks);
+        }
+
+        return charsetsList;
     }
 
 }
