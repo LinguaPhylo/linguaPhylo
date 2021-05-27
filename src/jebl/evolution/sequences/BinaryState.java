0d06b46f4f4c9382384044e4f30983f1bd5c8d4c
Walter <walterxie@users.noreply.github.com>
2021-05-28 09:42:25 +1200

    BinaryState #136

---
+++ src/jebl/evolution/sequences/BinaryState.java
@@ -0,0 +1,32 @@
+
+package jebl.evolution.sequences;
+
+import lphy.evolution.sequences.Binary;
+
+/**
+ * @author Walter Xie
+ */
+public final class BinaryState extends State {
+
+    public BinaryState(String name, String stateCode, int index) {
+        super(name, stateCode, index);
+    }
+
+    public BinaryState(String name, String stateCode, int index, BinaryState[] ambiguities) {
+        super(name, stateCode, index, ambiguities);
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        // throws ClassCastException on across-class comparison
+        BinaryState that = (BinaryState) o;
+        return super.compareTo(that);
+    }
+
+    public boolean isGap() {
+		return this == Binary.GAP_STATE;
+	}
+
+    public SequenceType getType() { return Binary.getInstance(); }
+
+}
--- src/lphy/evolution/sequences/Binary.java
+++ src/lphy/evolution/sequences/Binary.java
@@ -1,8 +1,11 @@
 
 package lphy.evolution.sequences;
 
+import jebl.evolution.sequences.BinaryState;
 import jebl.evolution.sequences.State;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -24,39 +27,39 @@ public final class Binary extends DataType {
 
     public static final String NAME = "binary";
 
-//    public static final State ZERO_STATE = new State("0", "0", 0);
-//    public static final State ONE_STATE = new State("1", "1", 1);
-//
-//    public static final State[] CANONICAL_STATES = new State[] {
-//            ZERO_STATE, ONE_STATE
-//    };
-//
-//    public static final State UNKNOWN_STATE = new State("Unknown base", "?", 2, CANONICAL_STATES);
-//    public static final State GAP_STATE = new State("Gap", "-", 3, CANONICAL_STATES);
-//
-//    public static final State[] STATES = new State[] {
-//            ZERO_STATE, ONE_STATE, UNKNOWN_STATE, GAP_STATE
-//    };
+    public static final BinaryState ZERO_STATE = new BinaryState("0", "0", 0);
+    public static final BinaryState ONE_STATE = new BinaryState("1", "1", 1);
+
+    public static final BinaryState[] CANONICAL_STATES = new BinaryState[] {
+            ZERO_STATE, ONE_STATE
+    };
+
+    public static final BinaryState UNKNOWN_STATE = new BinaryState("Unknown base", "?", 2, CANONICAL_STATES);
+    public static final BinaryState GAP_STATE = new BinaryState("Gap", "-", 3, CANONICAL_STATES);
+
+    public static final BinaryState[] STATES = new BinaryState[] {
+            ZERO_STATE, ONE_STATE, UNKNOWN_STATE, GAP_STATE
+    };
 
 
     @Override
     public int getStateCount() {
-        return 4;
+        return STATES.length;
     }
 
     @Override
     public List<State> getStates() {
-        return null;
+        return Collections.unmodifiableList(Arrays.asList((State[])STATES));
     }
 
     @Override
     public int getCanonicalStateCount() {
-        return 2;
+        return CANONICAL_STATES.length;
     }
 
     @Override
     public List<? extends State> getCanonicalStates() {
-        return null;
+        return Collections.unmodifiableList(Arrays.asList((State[])CANONICAL_STATES));
     }
 
     @Override
@@ -66,27 +69,27 @@ public final class Binary extends DataType {
 
     @Override
     public State getState(int index) {
-        return null;
+        return STATES[index];
     }
 
     @Override
     public State getUnknownState() {
-        return null;
+        return UNKNOWN_STATE;
     }
 
     @Override
     public State getGapState() {
-        return null;
+        return GAP_STATE;
     }
 
     @Override
     public boolean isUnknown(State state) {
-        return false;
+        return state == UNKNOWN_STATE;
     }
 
     @Override
     public boolean isGap(State state) {
-        return false;
+        return state == GAP_STATE;
     }
 
     @Override
