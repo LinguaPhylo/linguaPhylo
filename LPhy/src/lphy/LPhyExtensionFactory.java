793056cdf8e4749d88140a7b72b840164b8d4013
walterxie <walterxie@users.noreply.github.com>
2021-08-25 10:48:44 +1200

    comments on SPI

--- LPhy/src/lphy/LPhyExtensionFactory.java
+++ LPhy/src/lphy/LPhyExtensionFactory.java
@@ -11,33 +11,44 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 
 /**
+ * The implementation to load LPhy extensions using {@link ServiceLoader}.
+ * All distributions, functions and data types will be collected
+ * in this class for later use.
  * @author Walter Xie
  */
 public class LPhyExtensionFactory {
     private static LPhyExtensionFactory factory;
-//    private ServiceLoader<LPhyExtension> loader;
 
     private LPhyExtensionFactory() {
         ServiceLoader<LPhyExtension> loader = ServiceLoader.load(LPhyExtension.class);
 
-        registerTypes(loader);
+        registerExtensions(loader);
     }
-
+    // singleton
     public static synchronized LPhyExtensionFactory getInstance() {
-        if (factory == null) {
+        if (factory == null)
             factory = new LPhyExtensionFactory();
-        }
         return factory;
     }
 
+    /**
+     * {@link GenerativeDistribution}
+     */
     public Map<String, Set<Class<?>>> genDistDictionary;
+    /**
+     * {@link Func}
+     */
     public Map<String, Set<Class<?>>> functionDictionary;
-
+    /**
+     * LPhy data types
+     */
     public TreeSet<Class<?>> types = new TreeSet<>(Comparator.comparing(Class::getName));
-
+    /**
+     * LPhy sequence types {@link SequenceType}
+     */
     public Map<String, SequenceType> dataTypeMap;
 
-    private void registerTypes(ServiceLoader<LPhyExtension> loader) {
+    private void registerExtensions(ServiceLoader<LPhyExtension> loader) {
 
         genDistDictionary = new TreeMap<>();
         functionDictionary = new TreeMap<>();
@@ -48,9 +59,7 @@ public class LPhyExtensionFactory {
 
             while (extensions.hasNext()) { // TODO validation if add same name
 
-                /*
-                LPhyExtensionImpl must has a public no-args constructor
-                 */
+                //*** LPhyExtensionImpl must have a public no-args constructor ***//
                 LPhyExtension lPhyExt = extensions.next();
 
                 // GenerativeDistribution
@@ -61,11 +70,9 @@ public class LPhyExtensionFactory {
 
                     Set<Class<?>> genDistSet = genDistDictionary.computeIfAbsent(name, k -> new HashSet<>());
                     genDistSet.add(genClass);
-
+                    // collect LPhy data types from GenerativeDistribution
                     types.add(GenerativeDistribution.getReturnType(genClass));
-
                     Collections.addAll(types, Generator.getParameterTypes(genClass, 0));
-
                     Collections.addAll(types, Generator.getReturnType(genClass));
                 }
 //        for (Class<?> genClass : lightWeightGenClasses) {
@@ -73,10 +80,8 @@ public class LPhyExtensionFactory {
 //
 //            Set<Class<?>> genDistSet = genDistDictionary.computeIfAbsent(name, k -> new HashSet<>());
 //            genDistSet.add(genClass);
-//
 //            types.add(LGenerator.getReturnType((Class<LGenerator>)genClass));
 //        }
-
                 // Func
                 List<Class<? extends Func>> funcs = lPhyExt.getFunctions();
 
@@ -85,27 +90,26 @@ public class LPhyExtensionFactory {
 
                     Set<Class<?>> funcSet = functionDictionary.computeIfAbsent(name, k -> new HashSet<>());
                     funcSet.add(functionClass);
-
+                    // collect LPhy data types from Func
                     Collections.addAll(types, Generator.getParameterTypes(functionClass, 0));
-
                     Collections.addAll(types, Generator.getReturnType(functionClass));
                 }
 
-                // data types
+                // sequence types
                 Map<String, ? extends SequenceType> newDataTypes = lPhyExt.getSequenceTypes();
                 if (newDataTypes != null)
-                    // TODO validate same data type?
+                    // TODO validate same sequence type?
                     newDataTypes.forEach(dataTypeMap::putIfAbsent);
-
             }
 
             System.out.println("\nGenerativeDistribution : " + Arrays.toString(genDistDictionary.keySet().toArray()));
             System.out.println("Functions : " + Arrays.toString(functionDictionary.keySet().toArray()));
+            // for non-module release
+            if (genDistDictionary.size() < 1 || functionDictionary.size() < 1)
+                throw new RuntimeException("LPhy core did not load properly using SPI mechanism !");
 
             TreeSet<String> typeNames = types.stream().map(Class::getSimpleName).collect(Collectors.toCollection(TreeSet::new));
-
             System.out.println("LPhy data types : " + typeNames);
-
             System.out.println("LPhy sequence types : " + Arrays.toString(dataTypeMap.values().toArray(new SequenceType[0])));
 
         } catch (ServiceConfigurationError serviceError) {
