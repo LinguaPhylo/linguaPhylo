0bd9ef366b7a0d69e9162dde1a2b2911a481ff8a
walterxie <walter@cs.auckland.ac.nz>
2022-02-18 15:10:05 +1300

    update create project script

--- ext-manager/scripts/crProject.sh
+++ ext-manager/scripts/crProject.sh
@@ -36,8 +36,8 @@ version = "0.0.1-SNAPSHOT"
 base.archivesName.set("?")
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_16
-    targetCompatibility = JavaVersion.VERSION_16
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
     withSourcesJar()
 }
 
