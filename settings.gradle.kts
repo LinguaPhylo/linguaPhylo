fbd6d017a67f7a52eeac7263939b6c83e0a402c9
walterxie <walter@cs.auckland.ac.nz>
2022-03-29 15:22:25 +1300

    Disable build caching #170

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -28,10 +28,10 @@ pluginManagement {
 
 // https://docs.gradle.org/current/userguide/build_cache.html
 // https://docs.gradle.org/current/userguide/build_cache_use_cases.html
-buildCache {
-    local {
-        directory = File(rootDir, "build-cache")
-        removeUnusedEntriesAfterDays = 30
-        println("Creating local build cache : ${directory}")
-    }
-}
+//buildCache {
+//    local {
+//        directory = File(rootDir, "build-cache")
+//        removeUnusedEntriesAfterDays = 30
+//        println("Creating local build cache : ${directory}")
+//    }
+//}
