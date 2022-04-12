ad02ae10e23ae654131811e8a31f337653acef16
walterxie <walter@cs.auckland.ac.nz>
2022-04-12 15:38:07 +1200

    add package-info to lphy.core.lightweight.distributions

---
+++ lphy/src/main/java/lphy/core/lightweight/distributions/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * A lightweight version that involves only raw values for input and output
+ * and a beans style pattern for setting and getting.
+ * @deprecated
+ */
+package lphy.core.lightweight.distributions;
