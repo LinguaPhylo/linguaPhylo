30161018494a2714cbd54fd559fffb29bd1358a6
walterxie <walter@cs.auckland.ac.nz>
2022-04-20 12:47:27 +1200

    add package-info #136

---
+++ lphy/src/main/java/lphy/evolution/datatype/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * The implementations of {@link jebl.evolution.sequences.SequenceType}.
+ * They are used for importing sequences from files, and colouring alignments.
+ * @author Walter Xie
+ */
+package lphy.evolution.datatype;
