313ad289313bb89e7f5f1edffaf4576020851c11
walterxie <walter@cs.auckland.ac.nz>
2022-05-03 12:26:46 +1200

    one more in tree category #184

--- lphy/src/main/java/lphy/core/functions/NodeCount.java
+++ lphy/src/main/java/lphy/core/functions/NodeCount.java
@@ -1,10 +1,7 @@
 package lphy.core.functions;
 
 import lphy.evolution.tree.TimeTree;
-import lphy.graphicalModel.DeterministicFunction;
-import lphy.graphicalModel.GeneratorInfo;
-import lphy.graphicalModel.ParameterInfo;
-import lphy.graphicalModel.Value;
+import lphy.graphicalModel.*;
 import lphy.graphicalModel.types.IntegerValue;
 
 public class NodeCount extends DeterministicFunction<Integer> {
@@ -16,7 +13,9 @@ public class NodeCount extends DeterministicFunction<Integer> {
         setParam(paramName, x);
     }
 
-    @GeneratorInfo(name="nodecount",description = "The number of nodes in the tree")
+    @GeneratorInfo(name="nodecount",
+            category = GeneratorCategory.TREE, examples = {"yuleRelaxed.lphy"},
+            description = "The number of nodes in the tree")
     public Value<Integer> apply() {
         Value<TimeTree> v = (Value<TimeTree>)getParams().get(paramName);
         return new IntegerValue(v.value().getNodeCount(), this);
