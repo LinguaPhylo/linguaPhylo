eb57f44c4d6807836dbe2fbf60f4bd00e36535fd
walterxie <walter@cs.auckland.ac.nz>
2022-05-09 15:47:55 +1200

    export lphystudio packages #199

--- lphy-studio/src/main/java/module-info.java
+++ lphy-studio/src/main/java/module-info.java
@@ -3,10 +3,18 @@
  */
 module lphystudio {
     requires transitive lphy;
-
     requires transitive extmanager;
 
     requires jlatexmath;
+
+    exports lphystudio.app;
+    exports lphystudio.app.narrative;
+
+    exports lphystudio.core.codecolorizer;
+    exports lphystudio.core.layeredgraph;
+    exports lphystudio.core.log;
+    exports lphystudio.core.swing;
+    exports lphystudio.core.valueeditors;
 
     // declare what service interface the provider intends to use
     provides lphy.spi.LPhyExtension with lphystudio.spi.LPhyStudioImpl;
