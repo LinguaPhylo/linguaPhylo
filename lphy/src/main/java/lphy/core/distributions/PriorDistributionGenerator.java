c540c030eb9c59bf465cdc9f445c691e0c5038fc
walterxie <walterxie@users.noreply.github.com>
2022-06-03 10:00:32 +1200

    comments #152

--- lphy/src/main/java/lphy/core/distributions/PriorDistributionGenerator.java
+++ lphy/src/main/java/lphy/core/distributions/PriorDistributionGenerator.java
@@ -6,7 +6,14 @@ import lphy.util.RandomUtils;
 import org.apache.commons.math3.random.RandomGenerator;
 
 /**
- * Discrete distributions (Integer) and continuous distributions (Double)
+ * The template for discrete distributions (Integer) and continuous distributions (Double).
+ * Call super() to get random number generator,
+ * and constructDistribution() in the last line of constructor.
+ * This setParam handles all parameters automatically,
+ * so that the child class of prior distribution does not require to implement setParam() method.
+ * The example classes are Beta, Gamma, ...
+ * @author Walter Xie
+ * @author Alexei Drummond
  */
 public abstract class PriorDistributionGenerator<T> implements GenerativeDistribution<T> {
 
@@ -23,12 +30,18 @@ public abstract class PriorDistributionGenerator<T> implements GenerativeDistrib
     /**
      * Create the instance of distribution class given the parameter(s),
      * and cache it in order to reuse in sample() and density().
-     * It should be only called in constructor and setParam(),
-     * or any setters to change parameter value.
+     * It should be only called in the last line of constructor (required to call super()),
+     * and any additional setters to change parameter value.
      */
     protected abstract void constructDistribution(RandomGenerator random);
 
-
+    /**
+     * Call {@link lphy.graphicalModel.Generator#setParam(String, Value)}
+     * to set all parameters automatically, and call {@link #constructDistribution(RandomGenerator)}.
+     * So the implemented prior does not require to implement this method.
+     * @param paramName   parameter name
+     * @param value       {@link Value} contains parameter value.
+     */
     @Override
     public void setParam(String paramName, Value<?> value) {
         GenerativeDistribution.super.setParam(paramName, value);
