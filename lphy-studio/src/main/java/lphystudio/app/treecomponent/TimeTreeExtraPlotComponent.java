153c87a17b3c61fea179fa646faa986b046c26ec
walterxie <walter@cs.auckland.ac.nz>
2022-09-21 17:07:37 +1200

    working for birth-death tree #248

--- lphy-studio/src/main/java/lphystudio/app/treecomponent/TimeTreeExtraPlotComponent.java
+++ lphy-studio/src/main/java/lphystudio/app/treecomponent/TimeTreeExtraPlotComponent.java
@@ -2,7 +2,6 @@ package lphystudio.app.treecomponent;
 
 import lphy.evolution.tree.LTTUtils;
 import lphy.evolution.tree.TimeTree;
-import lphy.evolution.tree.TimeTreeNode;
 import lphystudio.app.Utils;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.JFreeChart;
@@ -16,8 +15,8 @@ import org.jfree.data.xy.XYSeriesCollection;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.Rectangle2D;
-import java.util.List;
-import java.util.*;
+import java.util.NavigableMap;
+import java.util.Objects;
 import java.util.prefs.Preferences;
 
 /**
@@ -38,31 +37,35 @@ public class TimeTreeExtraPlotComponent extends JComponent {
 
     private Rectangle2D bounds = new Rectangle2D.Double(0, 0, 1, 1);
 
-    TimeTreeComponent treeComponent;
-    XYDataset dataset;
+    final TimeTreeComponent treeComponent;
+    final XYDataset lttData;
 
     public TimeTreeExtraPlotComponent(TimeTreeComponent treeComponent) {
         this.treeComponent = treeComponent;
+        TimeTree tree = treeComponent.getTimeTree();
+        // compute data points here, not when paintComponent
+//        if (isLTTPlot()) {
+        this.lttData = computeLTT(tree);
+//        } else {
+//            neData = computeNe(tree);
+//        }
     }
 
     @Override
     public void paintComponent(Graphics g) {
 
         JFreeChart lineChart = null;
-        TimeTree tree = treeComponent.getTimeTree();
 
 //        if (isLTTPlot()) {
-            dataset = computeLTT(tree);
-
             lineChart = ChartFactory.createXYLineChart(
-                    null, xLable, yLTTLabel, dataset,
+                    null, xLable, yLTTLabel, lttData,
                     PlotOrientation.VERTICAL, false,true,false);
 //            lineChart.setTitle(new org.jfree.chart.title.TextTitle("LTT Plot", minPlotFont));
 //        } else {
-//            dataset = computeNe(tree);
+//            neData = computeNe(tree);
 //
 //            lineChart = ChartFactory.createXYLineChart(
-//                    null, xLable, yNeLabel, dataset,
+//                    null, xLable, yNeLabel, neData,
 //                    PlotOrientation.VERTICAL, false,true,false);
 //        }
         XYPlot plot = Objects.requireNonNull(lineChart).getXYPlot();
@@ -110,67 +113,26 @@ public class TimeTreeExtraPlotComponent extends JComponent {
         NavigableMap<Double, Integer> lttMap;
 
         if (tree.isUltrametric()) {
+            // faster
             lttMap = LTTUtils.getLTTFromUltrametricTree(tree);
-        } else { //TODO wrong for birth death tree
-            lttMap = new TreeMap<>();
-            List<TimeTreeNode> nodes = tree.getNodes();
-            // must add root first
-            lttMap.put(tree.getRoot().getAge(), 2);
-
-            // assuming the nodes list contains tips and then nodes, and the last is root.
-            for (int i = nodes.size(); i-- > 0; ) {
-                TimeTreeNode n = nodes.get(i);
-                // all ages > or == 0
-                final double age = n.getAge();
-                // skip if not root and age > 0
-                if (!n.isRoot() && age > 0) {
-
-                    if (lttMap.containsKey(age)) {
-                        // existing age
-                        final int l = lttMap.get(age);
-                        lttMap.put(age, l + 1);
-                    } else if (age < lttMap.firstKey()) {
-                        // new age, but younger (closer to 0) than all existing ages
-                        lttMap.put(age, lttMap.firstEntry().getValue() + 1);
-                    } else {
-                        // new age, but between existing ages
-                        for (var entry : lttMap.entrySet()) {
-                            // compare to existing ages in descending order
-                            final int l = lttMap.get(entry.getKey());
-                            if (age > entry.getKey()) {
-                                // +1 lineage for every existing ages if it is not leaf node
-                                // here leaf node age > 0
-                                if (!n.isLeaf())
-                                    lttMap.put(entry.getKey(), l + 1);
-                                else
-                                    lttMap.put(entry.getKey(), l - 1);
-                            } else {
-                                // add this new count
-                                if (!n.isLeaf())
-                                    // +1 lineage if it is not leaf node
-                                    lttMap.put(age, l + 1);
-                                else
-                                    // -1 lineage if it is leaf node
-                                    lttMap.put(age, l - 1);
-                                break;
-                            }
-                        }
-                    } // end if
-                } // end if isRoot
-            } // end for
-            System.out.println(Arrays.toString(nodes.stream().mapToDouble(TimeTreeNode::getAge).toArray()));
-
-        } // end if isUltrametric()
+        } else {
+            //TODO wrong for birth death tree
+            lttMap = LTTUtils.getLTTFromTimeTree(tree);
+        }
 
         System.out.println(lttMap);
 
         int preY = 1;
+        double age;
         // ages must be descending, otherwise plot is broken
         for (var entry : lttMap.descendingMap().entrySet()) {
             // add negative sign to ages
             // draw step line for ordinal numbers
-            series.add(entry.getKey() * -1, preY);
-            series.add(entry.getKey() * -1, entry.getValue());
+            age = entry.getKey();
+            series.add(age * -1, preY);
+            // do not draw vertical line in x==0
+            if (age > 0)
+                series.add(age * -1, entry.getValue());
             preY = entry.getValue();
         }
         dataset.addSeries(series);
--- lphy/src/main/java/lphy/evolution/tree/LTTUtils.java
+++ lphy/src/main/java/lphy/evolution/tree/LTTUtils.java
@@ -1,5 +1,6 @@
 package lphy.evolution.tree;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.NavigableMap;
 import java.util.TreeMap;
@@ -16,7 +17,7 @@ public class LTTUtils {
      *              whose key is age, and value is number of lineages.
      *              Note: before plotting by time in x-axis, ages need to multiply -1.
      */
-    public static NavigableMap<Double, Integer> getLTTFromUltrametricTree(TimeTree tree) {
+    public static NavigableMap<Double, Integer> getLTTFromUltrametricTree(final TimeTree tree) {
         if (tree.leafCount() < 3)
             throw new IllegalArgumentException("It is not a legal time tree !\n" +
                     "leaf count = " + tree.leafCount() + ", tree = " + tree);
@@ -35,7 +36,7 @@ public class LTTUtils {
             TimeTreeNode n = nodes.get(i);
             // all ages > or == 0
             final double age = n.getAge();
-            // skip if not root and age > 0
+            // proceed if not root and age > 0
             if (!n.isRoot() && age > 0) {
 
                 if (lttMap.containsKey(age)) {
@@ -57,7 +58,7 @@ public class LTTUtils {
                             lttMap.put(age, l + 1);
                             break;
                         }
-                    }
+                    } // end for
                 } // end if
 
             } // end if isRoot
@@ -66,4 +67,66 @@ public class LTTUtils {
         return lttMap;
     }
 
+
+    public static NavigableMap<Double, Integer> getLTTFromTimeTree(final TimeTree tree) {
+        // ordered age , lineages
+        NavigableMap<Double, Integer> lttMap = new TreeMap<>();
+        // include tips
+        List<TimeTreeNode> nodes = tree.getNodes();
+        // must add root first
+        lttMap.put(tree.getRoot().getAge(), 2);
+
+        // assuming the nodes list contains tips and then nodes, and the last is root.
+        for (int i = nodes.size(); i-- > 0; ) {
+            TimeTreeNode n = nodes.get(i);
+            // all ages > or == 0
+            final double age = n.getAge();
+            // proceed if not root
+            if (!n.isRoot()) {
+
+                if (lttMap.containsKey(age)) {
+                    // existing age
+                    final int l = lttMap.get(age);
+                    lttMap.put(age, l + 1);
+                } else if (age < lttMap.firstKey()) {
+                    // new age, but younger (closer to 0) than all existing ages
+                    // lineage of the youngest existing age
+                    final int l = lttMap.firstEntry().getValue();
+                    if (!n.isLeaf())
+                        // +1 if it is not leaf node
+                        lttMap.put(age,  l + 1);
+                    else
+                        // -1 if it is leaf node, in this case these leaf nodes are always younger than internal nodes
+                        lttMap.put(age,  l - 1);
+                } else {
+                    // new age, but between existing ages
+                    for (var entry : lttMap.entrySet()) {
+                        // compare to existing ages in descending order
+                        final int l = lttMap.get(entry.getKey());
+                        if (age > entry.getKey()) {
+                            // +1 lineage for every existing ages if it is not leaf node
+                            // here leaf node age > 0
+                            if (!n.isLeaf())
+                                lttMap.put(entry.getKey(), l + 1);
+                            else
+                                lttMap.put(entry.getKey(), l - 1);
+                        } else {
+                            // add this new count
+                            if (!n.isLeaf())
+                                // +1 lineage if it is not leaf node
+                                lttMap.put(age, l + 1);
+                            else
+                                // -1 lineage if it is leaf node
+                                lttMap.put(age, l - 1);
+                            break;
+                        }
+                    }
+                } // end if
+            } // end if isRoot
+        } // end for loop
+
+        System.out.println(Arrays.toString(nodes.stream().mapToDouble(TimeTreeNode::getAge).toArray()));
+
+        return lttMap;
+    }
 }
