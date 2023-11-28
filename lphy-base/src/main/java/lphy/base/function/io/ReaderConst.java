c8677f1571e2e8c55bef8bbe30acbecf9ff7423e
walterxie <walter@cs.auckland.ac.nz>
2023-11-29 10:37:55 +1300

    readTree function requires jebl3 to export jebl.evolution.trees.Tree #419

--- lphy-base/src/main/java/lphy/base/function/io/ReaderConst.java
+++ lphy-base/src/main/java/lphy/base/function/io/ReaderConst.java
@@ -4,6 +4,7 @@ public final class ReaderConst {
 
     public static final String FILE = "file";
     public static final String OPTIONS = "options";
+    public static final String FORMAT = "format";
 
     public static final String DELIMITER = "sep";
     public static final String HEADER = "header";
