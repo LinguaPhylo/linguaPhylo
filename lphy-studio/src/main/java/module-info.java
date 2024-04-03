d4a216ccf7211cc779388b21cd3d7015ba56e466
Walter <walterxie@users.noreply.github.com>
2024-04-04 11:24:06 +1300

    export required packages #453

--- lphy-studio/src/main/java/module-info.java
+++ lphy-studio/src/main/java/module-info.java
@@ -21,6 +21,12 @@ module lphystudio {
     exports lphystudio.core.valueeditor;
     exports lphystudio.core.theme;
 
+    //TODO how to extend panels ?
+    exports lphystudio.app.graphicalmodelpanel;
+    // for Viewer SPI
+    exports lphystudio.app.graphicalmodelpanel.viewer;
+    exports lphystudio.spi;
+
     // Both are empty now, but must be declared in order to show studio ext in the LPhyExtension Manager.
     // LPhy extensions
     uses lphy.core.spi.Extension;
