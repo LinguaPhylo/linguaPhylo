0954e79a4e9be3db47c28d0b3a48e2efafe0e12a
Walter <walterxie@users.noreply.github.com>
2024-05-31 12:51:49 +1200

    Use ParameterNames #281

--- lphy-base/src/main/java/lphy/base/function/Sort.java
+++ lphy-base/src/main/java/lphy/base/function/Sort.java
@@ -1,6 +1,5 @@
 package lphy.base.function;
 
-import lphy.base.ParameterNames;
 import lphy.core.model.DeterministicFunction;
 import lphy.core.model.Value;
 import lphy.core.model.annotation.GeneratorInfo;
@@ -9,12 +8,13 @@ import lphy.core.model.annotation.ParameterInfo;
 import java.util.Arrays;
 import java.util.Collections;
 
+import static lphy.base.ParameterNames.ArrayParamName;
+import static lphy.base.ParameterNames.DecreasingParamName;
+
 public class Sort<T> extends DeterministicFunction<T[]> {
 
-    private final String sortByParamName = ParameterNames.DecreasingParamName;
-
-    public Sort(@ParameterInfo(name = ParameterNames.ArrayParamName, description = "1d-array to sort.") Value<T[]> x,
-                @ParameterInfo(name = sortByParamName,
+    public Sort(@ParameterInfo(name = ArrayParamName, description = "1d-array to sort.") Value<T[]> x,
+                @ParameterInfo(name = DecreasingParamName,
             description = "sort the array by increasing (as default) or decreasing.",
                 optional = true) Value<Boolean> decreasing) {
 
@@ -23,16 +23,16 @@ public class Sort<T> extends DeterministicFunction<T[]> {
         if (value == null || value.length < 1)
             throw new IllegalArgumentException("Must have at least 1 element in the array!");
 
-        setParam(ParameterNames.ArrayParamName, x);
+        setParam(ArrayParamName, x);
         if (decreasing != null)
-            setParam(sortByParamName, decreasing);
+            setParam(DecreasingParamName, decreasing);
     }
 
     @GeneratorInfo(name = "sort", description = "The sort function sorts an array " +
             "by increasing (as default) or decreasing order.")
     public Value<T[]> apply() {
-        T[] arr = (T[]) getParams().get(ParameterNames.ArrayParamName).value();
-        Value<Boolean> decrVal = getParams().get(sortByParamName);
+        T[] arr = (T[]) getParams().get(ArrayParamName).value();
+        Value<Boolean> decrVal = getParams().get(DecreasingParamName);
         if (decrVal != null && decrVal.value())
             Arrays.sort(arr, Collections.reverseOrder());
         else
