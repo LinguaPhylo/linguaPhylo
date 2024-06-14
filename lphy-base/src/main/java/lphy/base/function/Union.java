ba7ce4e478b43bfc888086585a7a7d8f7a3937a5
yxu927 <132981793+yxu927@users.noreply.github.com>
2024-06-14 16:16:03 +1200

    let setUnion remove all repeats #482 #496

--- lphy-base/src/main/java/lphy/base/function/Union.java
+++ lphy-base/src/main/java/lphy/base/function/Union.java
@@ -7,47 +7,35 @@ import lphy.core.model.annotation.ParameterInfo;
 import lphy.core.parser.graphicalmodel.ValueCreator;
 
 import java.lang.reflect.Array;
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Union<T> extends DeterministicFunction<T[]> {
     public static final String firstSetName = "firstSet";
     public static final String secondSetName = "secondSet";
-    public static final String includeRepeatsName = "includeRepeats";
 
     public Union(
             @ParameterInfo(name = firstSetName, description = "the first set to put in the final set") Value<T[]> firstSet,
-            @ParameterInfo(name = secondSetName, description = "the second set to put in the final set") Value<T[]> secondSet,
-            @ParameterInfo(name = includeRepeatsName, description = "whether include the repeat elements, default false", optional = true) Value<Boolean> includeRepeats){
+            @ParameterInfo(name = secondSetName, description = "the second set to put in the final set") Value<T[]> secondSet){
         if (firstSet == null) throw new IllegalArgumentException("The first set should not be null!");
         if (secondSet == null) throw new IllegalArgumentException("The second set should not be null!");
         setParam(firstSetName, firstSet);
         setParam(secondSetName, secondSet);
-        setParam(includeRepeatsName, includeRepeats);
     }
 
-    @GeneratorInfo(name = "setUnion", description = "Set the union of all given sets. Default remove all repeat elements.")
+    @GeneratorInfo(name = "setUnion", description = "Set the union of two given sets and remove all repeat elements.")
     @Override
     public Value<T[]> apply() {
-        T[] firstSet = (T[]) getParams().get(firstSetName).value();
-        T[] secondSet = (T[]) getParams().get(secondSetName).value();
+        Set<T> firstSet = new HashSet<>(Arrays.asList(((Value<T[]>) getParams().get(firstSetName)).value()));
+        Set<T> secondSet = new HashSet<>(Arrays.asList(((Value<T[]>) getParams().get(secondSetName)).value()));
 
-        Value<Boolean> includeRepeatsValue = getParams().get(includeRepeatsName);
-        Boolean includeRepeats = (includeRepeatsValue != null && includeRepeatsValue.value() != null) ? includeRepeatsValue.value() : false;
-
-        // add both set info
-        List<T> combinedList = new ArrayList<>();
-        combinedList.addAll(Arrays.asList(firstSet));
-        combinedList.addAll(Arrays.asList(secondSet));
-
-        if ( !includeRepeats){
-            //remove repeats
-            Set<T> uniqueSet = new HashSet<>(combinedList);
-            combinedList = new ArrayList<>(uniqueSet);
-        }
+        // combine the two sets
+        firstSet.addAll(secondSet);
 
         // convert to array
-        T[] resultArray = (T[]) Array.newInstance(firstSet.getClass().getComponentType(), combinedList.size());
-        combinedList.toArray(resultArray);
+        T[] resultArray = (T[]) Array.newInstance(((Value<T[]>) getParams().get(firstSetName)).value().getClass().getComponentType(), firstSet.size());
+        firstSet.toArray(resultArray);
 
         return ValueCreator.createValue(resultArray, this);
     }
--- lphy-base/src/test/java/lphy/base/function/UnionTest.java
+++ lphy-base/src/test/java/lphy/base/function/UnionTest.java
@@ -3,8 +3,6 @@ package lphy.base.function;
 import lphy.core.model.Value;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class UnionTest {
@@ -15,7 +13,7 @@ public class UnionTest {
         Value<String[]> firstSet = new Value<>("firstSet", set1);
         Value<String[]> secondSet = new Value<>("secondSet", set2);
 
-        Union instance = new Union(firstSet,secondSet,null);
+        Union instance = new Union(firstSet,secondSet);
         String[] observe = (String[]) instance.apply().value();
         String[] expect = {"1","2","3","4","5"};
         for (int i = 0; i < observe.length; i++) {
@@ -30,30 +28,9 @@ public class UnionTest {
         Value<Integer[]> firstSet = new Value<>("firstSet", set1);
         Value<Integer[]> secondSet = new Value<>("secondSet", set2);
 
-        Union instance = new Union(firstSet,secondSet,null);
+        Union instance = new Union(firstSet,secondSet);
         Integer[] observe = (Integer[]) instance.apply().value();
         Integer[] expect = {1,2,3,4,5};
-        for (int i = 0; i < observe.length; i++) {
-            assertEquals(expect[i], observe[i]);
-        }
-    }
-
-    @Test
-    void applyTest3() {
-        Integer[] set1 = {1,2,3};
-        Integer[] set2 = {3,4,5};
-        Value<Integer[]> firstSet = new Value<>("firstSet", set1);
-        Value<Integer[]> secondSet = new Value<>("secondSet", set2);
-        Value<Boolean> includeRepeats = new Value<>("includeRepeats", true);
-
-        Union instance = new Union(firstSet,secondSet,includeRepeats);
-        Integer[] observe = (Integer[]) instance.apply().value();
-        Integer[] expect = {1,2,3,3,4,5};
-        Arrays.sort(observe);
-        Arrays.sort(expect);
-
-        assertEquals(expect.length, observe.length);
-
         for (int i = 0; i < observe.length; i++) {
             assertEquals(expect[i], observe[i]);
         }
