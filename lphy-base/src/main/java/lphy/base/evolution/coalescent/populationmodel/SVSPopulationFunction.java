32b0990e9bc56709e4a5e4ebd90a759cde2bc427
yxu927 <132981793+yxu927@users.noreply.github.com>
2024-07-15 15:36:16 +1200

    add SVSPopulationFunctionToBEAST

--- lphy-base/src/main/java/lphy/base/evolution/coalescent/populationmodel/SVSPopulationFunction.java
+++ lphy-base/src/main/java/lphy/base/evolution/coalescent/populationmodel/SVSPopulationFunction.java
@@ -1,6 +1,12 @@
 package lphy.base.evolution.coalescent.populationmodel;
 
 import lphy.base.evolution.coalescent.PopulationFunction;
+import lphy.core.model.Value;
+
+import java.util.Map;
+
+import static lphy.base.evolution.coalescent.populationmodel.SVSFunction.INDICATOR_PARAM_NAME;
+import static lphy.base.evolution.coalescent.populationmodel.SVSFunction.MODELS_PARAM_NAME;
 
 public class SVSPopulationFunction implements PopulationFunction {
 
@@ -34,4 +40,17 @@ public class SVSPopulationFunction implements PopulationFunction {
     public PopulationFunction getModel() {
         return model;
     }
+
+    public Value getIndicator() {
+        return (Value<Integer>) getParams().get(INDICATOR_PARAM_NAME);
+    }
+
+    private Map<Object, Object> getParams() {
+        return null;
+    }
+
+    public Value<PopulationFunction[]> getModels() {
+        return (Value<PopulationFunction[]>) getParams().get(MODELS_PARAM_NAME);
+    }
+
 }
