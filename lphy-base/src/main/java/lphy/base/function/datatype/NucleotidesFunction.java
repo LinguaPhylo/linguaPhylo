8b3878909b37a83a298d260045828c69de25e167
EvaLiyt <liyutong0617@gmail.com>
2024-07-17 15:30:45 +1200

    change example for NucleotidesFunction()

--- lphy-base/src/main/java/lphy/base/function/datatype/NucleotidesFunction.java
+++ lphy-base/src/main/java/lphy/base/function/datatype/NucleotidesFunction.java
@@ -11,7 +11,7 @@ public class NucleotidesFunction extends DeterministicFunction<SequenceType> {
     public NucleotidesFunction() {}
 
     @GeneratorInfo(name = "nucleotides", verbClause = "is", narrativeName = "nucleotide data type",
-            category = GeneratorCategory.SEQU_TYPE, examples = {"primates2.lphy"},
+            category = GeneratorCategory.SEQU_TYPE, examples = {"simpleSerialCoalescentNex.lphy"},
             description = "The nucleotide data type.")
     public Value<SequenceType> apply() {
         return new Value<>(null, SequenceType.NUCLEOTIDE, this);
