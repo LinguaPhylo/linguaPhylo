37825d53ddf631bfc467a8e936d52a7d1b652a1e
Walter <walterxie@users.noreply.github.com>
2025-08-14 16:04:36 +1200

    fix Beta test #561

--- lphy-base/src/test/java/lphy/base/distribution/BetaTest.java
+++ lphy-base/src/test/java/lphy/base/distribution/BetaTest.java
@@ -7,10 +7,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.phylospec.types.PositiveReal;
+import org.phylospec.types.impl.PositiveRealImpl;
 
 import java.util.stream.Stream;
 
-import static lphy.core.model.ValueUtils.doubleValue;
+import static lphy.core.model.ValueUtils.doublePrimitiveValue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
@@ -19,10 +21,12 @@ public class BetaTest {
     // alpha, beta, expected log density
     static Stream<Arguments> betaArgProvider() {
         return Stream.of(
-                arguments(new Value<>(null, 5.1), new Value<>(null, 1.9),
+                arguments(new Value<>(null, new PositiveRealImpl(5.1)),
+                        new Value<>(null, new PositiveRealImpl(1.9)),
                         // expected log density dbeta(seq(0,1,by=.2), 5.1, 1.9, log = T)
                         new double[]{Double.NEGATIVE_INFINITY,-3.5110533,-0.9280637,0.3694246,0.9250886,Double.NEGATIVE_INFINITY}),
-                arguments(new Value<>(null, 2), new Value<>(null, 6),
+                arguments(new Value<>(null, new PositiveRealImpl(2)),
+                        new Value<>(null, new PositiveRealImpl(6)),
                         // expected log density
                         new double[]{Double.NEGATIVE_INFINITY,1.0125139, 0.2672508,-1.3546097,-4.5326635,Double.NEGATIVE_INFINITY})
         );
@@ -35,18 +39,18 @@ public class BetaTest {
 
     @ParameterizedTest
     @MethodSource("betaArgProvider")
-    public void sample(Value<Number> alpha, Value<Number> beta) {
+    public void sample(Value<PositiveReal> alpha, Value<PositiveReal> beta) {
         System.out.println("alpha = " + alpha + ", beta = " + beta);
         Beta bt = new Beta(alpha, beta);
 
         SummaryStatistics summ = new SummaryStatistics();
         for (int i = 0; i < 10000; i++) {
-            summ.addValue(bt.sample().value());
+            summ.addValue(bt.sample().value().getPrimitive());
         }
         double m = summ.getMean();
         double var = summ.getVariance();
-        double a = doubleValue(alpha);
-        double b = doubleValue(beta);
+        double a = doublePrimitiveValue(alpha);
+        double b = doublePrimitiveValue(beta);
 
         // https://en.wikipedia.org/wiki/Beta_distribution
         double expectedMean =  a / (a + b);
@@ -62,7 +66,7 @@ public class BetaTest {
 
     @ParameterizedTest
     @MethodSource("betaArgProvider")
-    public void logDensity(Value<Number> alpha, Value<Number> beta, double[] logDensityArr) {
+    public void logDensity(Value<PositiveReal> alpha, Value<PositiveReal> beta, double[] logDensityArr) {
         System.out.println("alpha = " + alpha + ", beta = " + beta);
         Beta bt = new Beta(alpha, beta);
         // 0 to 1 step .2
