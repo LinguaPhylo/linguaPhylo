28f444a41df2a7b2ea0cde657fb57a0d2e762558
Yao Xiao <xbarry1818@gmail.com>
2026-01-23 15:36:47 +1300

    made the return of ReadMpileup.java.java to be Mpileup.java instead of List of Mpileup.java #569

--- lphy-base/src/test/java/lphy/base/function/io/MpileupTest.java
+++ lphy-base/src/test/java/lphy/base/function/io/MpileupTest.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -18,7 +19,6 @@ public class MpileupTest {
     void testReading() throws IOException {
         // make files up
         Path mpileup = Files.createTempFile("mpileup", ".mpileup");
-        Path cellName = Files.createTempFile("cell", ".txt");
 
         List<String> lines = List.of(
                 "chr1\t1\tG\t18\t..................\t~~~~~~~~~~~~~~~~~~\t0\t*\t~~~~~~~~~~\t3\t...\t~~~\t80\t................................................................................\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\t7\t.......\t~~~~~~~\t1\t.\t~\t3\t..T\t~~~\t8\t........\t~~~~~~~~\t7\t.......\t~~~~~~~\t31\t...............................\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\t6\t......\t~~~~~~\t3\t...\t~~~\t2\t..\t~~\t9\t.........\t~~~~~~~~~\t5\t.....\t~~~~~\t5\t.....\t~~~~~\t0\t*\t~~~~~~~~~~\t16\t................\t~~~~~~~~~~~~~~~~\t6\t......\t~~~~~~\t13\t.............\t~~~~~~~~~~~~~\t36\t....................................\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\t5\t.....\t~~~~~\t10\t..........\t~~~~~~~~~~\t7\t.......\t~~~~~~~\t4\t....\t~~~~\t10\t..........\t~~~~~~~~~~\t21\t.....................\t~~~~~~~~~~~~~~~~~~~~~\t12\t............\t~~~~~~~~~~~~\t22\t......................\t~~~~~~~~~~~~~~~~~~~~~~\t13\t.............\t~~~~~~~~~~~~~\t0\t*\t~~~~~~~~~~\t28\t............................\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~\t4\t....\t~~~~\t4\t....\t~~~~\t3\t...\t~~~\t5\t.....\t~~~~~\t4\t....\t~~~~\t0\t*\t~~~~~~~~~~\t3\t...\t~~~\t1\t.\t~",
@@ -26,40 +26,40 @@ public class MpileupTest {
         );
         Files.write(mpileup, lines);
 
-        List<String> names = IntStream.rangeClosed(1, 40)
-                .mapToObj(Integer::toString)
-                .collect(Collectors.toList());
-        Files.write(cellName, names);
+        String[] names = new String[40];
+        for (int i = 0; i < 40; i++) {
+            names[i] = String.valueOf(i);
+        }
 
         // read in made up files
         Value<String> mpileupValue = new Value<>("", mpileup.toString());
-        Value<String> cellNameValue = new Value<>("", cellName.toString());
+        Value<Object[]> cellNameValue = new Value<>("", names);
         ReadMpileup reader = new ReadMpileup(mpileupValue, cellNameValue);
-        List<Mpileup> results = reader.apply().value();
+        Mpileup results = reader.apply().value();
 
         // check the results
-        assertEquals(2, results.size());
+        assertEquals(2, results.getPileupData().size());
 
         // check mpileup cell num
-        assertEquals(40, results.get(0).getPileupData().size());
-        assertEquals(40, results.get(1).getPileupData().size());
+        assertEquals(40, results.getPileupData().get(0).size());
+        assertEquals(40, results.getPileupData().get(1).size());
 
         // check 3 columns
-        assertEquals("chr1", results.get(0).getChromName());
-        assertEquals(1, results.get(0).getPosition());
-        assertEquals(2, results.get(0).getRef());
+        assertEquals("chr1", results.getChromNames()[0]);
+        assertEquals(1, results.getPositions()[0]);
+        assertEquals(2, results.getRefs()[0]);
 
-        assertEquals("chr1", results.get(1).getChromName());
-        assertEquals(2, results.get(1).getPosition());
-        assertEquals(0, results.get(1).getRef());
+        assertEquals("chr1", results.getChromNames()[1]);
+        assertEquals(2, results.getPositions()[1]);
+        assertEquals(0, results.getRefs()[1]);
     }
 
     @Test
     void testInvalidFormat() throws IOException {
         Path mpileup = Files.createTempFile("mpileup", ".txt");
-        Path cellName = Files.createTempFile("cellName", ".pileup");
         Value<String> mpileupValue = new Value<>("", mpileup.toString());
-        Value<String> cellNameValue = new Value<>("", cellName.toString());
+        String[] cellNames = new String[]{"Cell1", "Cell2"};
+        Value<Object[]> cellNameValue = new Value<>("", cellNames);
         IllegalArgumentException thrown = assertThrows(
                 IllegalArgumentException.class,
                 () -> new ReadMpileup(mpileupValue, cellNameValue),
